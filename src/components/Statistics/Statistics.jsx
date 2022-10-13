e00eafcb93934afc8a062e27acb494223f48df75
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-10-13 21:12:33 +0200

    diff bg colors- fixed
    diff bg colors- fixed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,17 @@
 //import PropTypes from 'prop-types';
-import Stats from './statistics.module.css'
+import Stats from './statistics.module.css';
 
 export const UserStats = ({ title, stats }) => {
   return (
     <section className={Stats.statistics}>
-          { title && <h2 class={Stats.title}>{title}</h2>}
+      {title && <h2 class={Stats.title}>{title}</h2>}
       <ul className={Stats.statList}>
         {stats.map(({ id, label, percentage }) => (
-         <li className={Stats.item} key={id} style={{ backgroundColor:colorHex }}>
+          <li
+            className={Stats.item}
+            key={id}
+            style={{ backgroundColor: colorHex() }}
+          >
             <span className={Stats.label}>{label}</span>
             <span className={Stats.percentage}>{percentage}%</span>
           </li>
@@ -17,4 +21,6 @@ export const UserStats = ({ title, stats }) => {
   );
 };
 
-const colorHex = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+const colorHex = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
